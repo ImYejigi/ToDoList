acb9a7e1e555999f14cab878485f86e9179b19b3
ImYejigi <jyj000207@gmail.com>
2023-03-21 22:43:14 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+
+  let [todo,setTodo] = useState("");
+  let [todos,setTodos] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(todo === ""){
+        return
+    }
+    setTodos((currentArray) =>[todo, ...currentArray])
+    setTodo("");
+    
+  } 
+  const onChange = (e) => {
+    setTodo(e.target.value)
+    console.log(todo)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1>투두리스트 만들기</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder='목표 입력' onChange={onChange} value={todo}/>
+          <button>설정하기</button>
+      </form>
+      <ul>
+        {todos.map((item,index)=><li key={index}>{item}</li>)}
+      
+      </ul>
+        
+     </div>
+    
+    );
+  }
 
 export default App;
